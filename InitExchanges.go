211afd4dd94339bf70a2e73534ca996232e5973b
laurensKubat <laurensKubat@gmail.com>
2018-05-07 15:47:08 +0200

    Binance working, Huobi stalls for some reason

--- InitExchanges.go
+++ InitExchanges.go
@@ -6,7 +6,6 @@ func initHuobi( out chan ListenOut, stop chan bool){
 	c := NewClient(HuobiHandler{})
 	c.start("wss://api.huobi.pro/ws", "market.ethusdt.trade.detail", "id1", out, stop)
 	go Printer(out, stop)
-
 }
 
 
@@ -15,5 +14,4 @@ func initBinance(out chan ListenOut, stop chan bool) {
 	c := NewClient(BinanceHandler{},)
 	c.start("wss://stream.binance.com:9443/ws/", "bnbbtc@trade","id1", out, stop)
 	go Printer(out, stop)
-
 }
